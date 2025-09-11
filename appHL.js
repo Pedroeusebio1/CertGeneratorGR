cd849d51b8e68f9d4b363144c650dfffbf9a0b0e
Pedroeusebio1 <154755501+Pedroeusebio1@users.noreply.github.com>
2025-09-11 12:47:25 -0400

    Update appHL.js

--- appHL.js
+++ appHL.js
@@ -23,7 +23,8 @@ if (localStorage.getItem('club') === 'HL') {
     setTimeout(() => {
 
     localStorage.clear();
-    window.location.href = '/HTML/index.html';
+    window.location.href = '/index.html';
     
     }, 3000);
+
 }
